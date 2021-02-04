2d3ff4e37111da3c52a1e876a5a7919462a2de6e
diegofs29 <diegofdezsrz12@gmail.com>
2021-02-04 12:38:32 +0100

    Creacion proyecto

---
+++ virtualbank/src/test/java/com/bank/virtualbank/VirtualbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.virtualbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
